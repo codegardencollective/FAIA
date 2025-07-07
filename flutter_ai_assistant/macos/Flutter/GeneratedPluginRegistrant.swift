c4b2682c6e0f717b678023d32ccf7fde867d79e3
Fernando May <fmay130@gmail.com>
2025-07-06 20:52:48 -0600

    Refactor project dependencies and add platform-specific configurations
    - Commented out TensorFlow Lite dependencies in pubspec.yaml.
    - Registered new plugins (BatteryPlus, FlutterTts, PermissionHandler) in generated_plugin_registrant.cc for Windows.
    - Updated generated_plugins.cmake to include new plugins.
    - Created Podfile for iOS with necessary configurations for Flutter integration.
    - Created Podfile for macOS with necessary configurations for Flutter integration.

--- flutter_ai_assistant/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter_ai_assistant/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,18 @@
 import FlutterMacOS
 import Foundation
 
+import battery_plus
+import device_info_plus
+import flutter_tts
+import path_provider_foundation
+import shared_preferences_foundation
+import speech_to_text_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  BatteryPlusMacosPlugin.register(with: registry.registrar(forPlugin: "BatteryPlusMacosPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
 }
